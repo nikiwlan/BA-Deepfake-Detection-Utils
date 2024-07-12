d8e033ca4654c461a89c7f5924399b3c75d2dfc1
Nilanges <65857504+nikiwlan@users.noreply.github.com>
2024-07-12 13:57:43 +0200

    Update detect_video_landmarks.py

--- Datasets/preprocess_datasets/detect68landmarks/detect_video_landmarks.py
+++ Datasets/preprocess_datasets/detect68landmarks/detect_video_landmarks.py
@@ -11,9 +11,9 @@ def calculate_percent_value(array):
     percent_value = float(tensor[0]) * 100
     return percent_value
 
-def process_videos(fa, input_folder_name, base_dir):
-    input_dir = os.path.join(base_dir, f"{input_folder_name}\\c23\\videos")
-    output_dir = os.path.join(base_dir, f"{input_folder_name}\\c23\\landmarks")
+def process_videos(fa, base_dir, dataset_name, subdataset_name):
+    input_dir = os.path.join(base_dir, dataset_name, subdataset_name, "c23", "videos")
+    output_dir = os.path.join(base_dir, dataset_name, subdataset_name, "c23", "landmarks")
 
     print(f"Processing videos in directory: {input_dir}")
     print(f"Saving landmarks to directory: {output_dir}")
@@ -22,7 +22,7 @@ def process_videos(fa, input_folder_name, base_dir):
 
     for value in values_to_calculate:
         percent_value = calculate_percent_value(np.array([value]))
-        print("Für den Wert", value, "ist der prozentuale Wert:", percent_value)
+        print(f"Für den Wert {value} ist der prozentuale Wert: {percent_value:.2f}")
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -46,7 +46,7 @@ def process_videos(fa, input_folder_name, base_dir):
                     print("Keine weiteren Frames im Video vorhanden.")
                     break
 
-                print("Verarbeite Frame im Video:", video_path)
+                print(f"Verarbeite Frame im Video: {video_path}")
 
                 preds = fa.get_landmarks(frame)
 
@@ -65,17 +65,15 @@ def process_videos(fa, input_folder_name, base_dir):
 def main():
     parser = argparse.ArgumentParser(description="Process videos for face landmarks.")
     parser.add_argument('--base_dir', type=str, required=True, help='Base directory containing the video folders.')
+    parser.add_argument('--dataset_name', type=str, required=True, help='Dataset name (e.g., Forensics)')
+    parser.add_argument('--subdataset_name', type=str, required=True, help='Subdataset name (e.g., RealFF)')
     args = parser.parse_args()
 
     # Initialize the face alignment model
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType.TWO_D, flip_input=False, face_detector='blazeface')
 
-    # List of input folder names
-    input_folders = ["Real", "Deepfakes", "FaceSwap", "Face2Face", "NeuralTextures", "FaceShifter", "DeepFakeDetection"]
-
-    # Process each folder
-    for folder in input_folders:
-        process_videos(fa, folder, args.base_dir)
+    # Process the given subdataset
+    process_videos(fa, args.base_dir, args.dataset_name, args.subdataset_name)
 
 if __name__ == "__main__":
     main()
